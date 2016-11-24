ffeb8e69a8bfb424cfc83e68433ff9bea7a82a20
scottlepp <scott.lepper@gmail.com>
2016-11-24 11:08:14 -0500

    initial commit

---
+++ Bailout/src/pages/bond/bond.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-bond',
+  templateUrl: 'bond.html'
+})
+export class BondPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
